80bc54fce7a6a755a2384c8bfccf33f1fadcf472
Kundan Bhardwaj <kundanbhardwaj145@gmail.com>
2021-06-20 13:46:48 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JS_and_TS_notes
 # JS_and_TS_notes
 # JS_and_TS_notes
+# JS_and_TS_notes
---
+++ arrow function.ts
@@ -0,0 +1,7 @@
+// program to find the area of a circle in ts and js 
+// arrow function in ts and js 
+
+area = (radius) => "the area of the circle is " + Math.PI*radius*radius;
+// using the arrow function
+console.log(area(56));
+
